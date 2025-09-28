c8f5369f4259fa5a1227caf7088c2943872d226e
JoseCode <jose.malafaiacode@outlook.com>
2025-09-28 18:05:44 -0300

    implementar interface gráfica

---
+++ yolo_interface.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
+import subprocess
+from yolo_process import *
+from yolo_model_train import *
+from yolo_feed import *
+
+class YOLOInterface:
+    def __init__(self, feed_func, train_func, output_dir="./datasets/processed"):
+        """
+        Classe da interface gráfica para YOLOv8
+        :param root: janela Tkinter principal
+        :param feed_func: função para abrir feed de vídeo
+        :param train_func: função para treinar modelo
+        :param output_dir: diretório de saída das imagens processadas
+        """
+        self.root = tk.Tk()
+        self.feed_func = feed_func
+        self.train_func = train_func
+        self.output_dir = output_dir
+        self.feed_obj = YOLOfeed()
+        self.model_obj = YOLOModelTrain()
+        self.root.title("Interface YOLOv8")
+        self.root.geometry("400x200")
+
+        # Botões principais
+        btn_process = tk.Button(self.root, text="Processar Imagens", command=self.open_process_window, width=30, height=2)
+        btn_process.pack(pady=10)
+
+        btn_feed = tk.Button(self.root, text="Abrir Feed de Vídeo", command=self.open_feed_window, width=30, height=2)
+        btn_feed.pack(pady=10)
+
+        btn_train = tk.Button(self.root, text="Treinar Modelo", command=self.open_train_window, width=30, height=2)
+        btn_train.pack(pady=10)
+
+    # =============================
+    # Janelas auxiliares
+    # =============================
+    def open_process_window(self):
+        input_dir = filedialog.askdirectory(title="Selecione a pasta com imagens")
+        model_path = self.model_obj.get_latest_model()
+        if not input_dir:
+            return
+        try:
+            process_result = YOLOProcess(model_path=model_path,input_dir=input_dir,output_dir=".\\datasets\\processed").process_images()
+            messagebox.showinfo("Sucesso", process_result)
+
+            # Abre a pasta de saída no Explorer
+            subprocess.Popen(f'explorer "{os.path.abspath(self.output_dir)}"')
+        except Exception as e:
+            messagebox.showerror("Erro", f"Erro ao processar imagens:\n{str(e)}")
+
+    def open_feed_window(self):
+        self.feed_obj.app.run(host="0.0.0.0", port=5000, debug=False)
+
+    def open_train_window(self):
+        self.model_obj.run_training()
---
+++ yolo_model_train.py
@@ -0,0 +1,70 @@
+from ultralytics import YOLO
+import cv2
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+import torch
+import shutil
+
+class YOLOModelTrain:
+    def __init__(self, models_dir="model_history", max_trainings=50):
+        self.models_dir = models_dir
+        self.max_trainings = max_trainings
+
+    def get_latest_model(self):
+        subdirs = [os.path.join(self.models_dir, d) for d in os.listdir(self.models_dir) if os.path.isdir(os.path.join(self.models_dir, d))]
+        if not subdirs:
+            return "best.pt"
+        latest = max(subdirs, key=os.path.getmtime)
+        file_path = os.path.join(latest, "weights", "best.pt")
+        return file_path
+
+    def clean_old_trainings(self):
+        """
+        Mantém apenas os últimos `max_trainings` diretórios dentro de models_dir.
+        Remove os mais antigos até restarem exatamente `max_trainings`.
+        """
+        subdirs = [os.path.join(self.models_dir, d) for d in os.listdir(self.models_dir)
+                if os.path.isdir(os.path.join(self.models_dir, d))]
+
+        if len(subdirs) <= self.max_trainings:
+            print("Não foram apagados modelos do historico")
+            return
+
+        subdirs.sort(key=os.path.getmtime)
+        excess = len(subdirs) - self.max_trainings
+        for i in range(excess):
+            old_dir = subdirs[i]
+            print(f"[INFO] Apagando treino antigo: {old_dir}")
+            shutil.rmtree(old_dir, ignore_errors=True)
+
+    def train_model(self):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+        project_dir = self.models_dir#".\\model_history"
+        exp_name = f"epi_yolov8_{timestamp}"
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        device_str = ""
+        os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+        if torch.cuda.is_available():
+            device_str = "cuda"
+        else:
+            device_str = "cpu"
+        device = torch.device(device=device_str)
+        model = YOLO(self.get_latest_model())
+        model.to(device)
+        model.train(
+            data=f"{script_dir}\\data.yaml",
+            epochs=10,
+            imgsz=640,
+            batch=16,
+            name=exp_name,
+            project=project_dir,
+            device=0
+        )
+    
+    def run_training(self):
+        self.clean_old_trainings()
+        self.train_model()
+
+
+
